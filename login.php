a045b5e1c7d879c7b668e6daeadfb194eee69f43
Dekaver <58721541+Dekaver@users.noreply.github.com>
2020-10-14 19:58:24 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/login.css">
     <title>Document</title>
 </head>
 
 <body>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <form class="container"  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <img src="img/Logo-Web-ITK.png" alt="itk" id="itk">
+    <img src="img/if-nav.png" alt="Informatika" id="informatika">
+
         <fieldset>
-            <legend>Login</legend>
             <table>
                 <tr>
                     <td>Username</td>
@@ -26,20 +29,16 @@
                 <tr>
                     <td></td>
                     <td></td>
-                    <td><input type="submit" value="Submit">
-                        <input type="reset" value="Reset"></td>
+                    <td><input class="login" type="submit" value="Login">
+                        <input class="reset" type="reset" value="Reset"></td>
                 </tr>
             </table>
         </fieldset>
-    </form>
-</body>
-<?php
+        <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mysqli = new mysqli("localhost", "root", "", "database");
-
     $sql = "SELECT * FROM user where username='" . $_POST['username'] . "' and password='" . $_POST['password'] . "'";
     $result = $mysqli->query($sql);
-
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             session_start();
@@ -48,9 +47,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("Location: index.php");
         }
     } else {
-        echo "Username dan Password salah";
+        echo "<h4 id='red'>Incorrect Password or Username</h4><style>#red{color:red;}fieldset{border:3px solid red;}</style>";
     }
 }
 ?>
+    </form>
+    
 
+
+</body>
 </html>
